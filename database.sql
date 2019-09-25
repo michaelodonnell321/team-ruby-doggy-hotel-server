a67696c6d0105f0e86cd01c4106d4b43bd436182
Michael O'Donnell <gorillaejones@gmail.com>
2019-09-25 16:13:06 -0500

    database.sql with table creation instructions added

---
+++ database.sql
@@ -0,0 +1,20 @@
+-- use postico to create these tables
+-- in a database named
+-- pet_hotel
+
+CREATE TABLE "owners"
+(
+    id serial PRIMARY KEY,
+    "name" VARCHAR (100),
+    "number_of_pets" INT
+);
+
+CREATE TABLE "pets"
+(
+    id serial PRIMARY KEY,
+    "owner_id" INT REFERENCES "owners",
+    "pet" VARCHAR (100),
+    "breed" VARCHAR (100),
+    "color" VARCHAR (50),
+    "checked_in" BOOLEAN
+);
